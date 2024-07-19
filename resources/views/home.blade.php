fea86ebbb15574a7ecbcbad2b82ea50b20e120b4
tawfeeqtech <86443941+tawfeeqtech@users.noreply.github.com>
2024-07-19 17:22:49 +0300

    add profile page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
         <div class="page-header">
             <div class="row">
                 <div class="col-sm-12 mt-5">
-                    <h3 class="page-title mt-3">Good Morning Soeng Souy!</h3>
+                    <h3 class="page-title mt-3">Good Morning {{Auth::user()->name}}!</h3>
                     <ul class="breadcrumb">
                         <li class="breadcrumb-item active">Dashboard</li>
                     </ul>
--- resources/views/layouts/base/_header.blade.php
+++ resources/views/layouts/base/_header.blade.php
@@ -75,10 +75,10 @@
                 <div class="user-header">
                     <div class="avatar avatar-sm"> <img src="{{asset('assets/img/profiles/avatar-01.jpg')}}" alt="User Image" class="avatar-img rounded-circle"> </div>
                     <div class="user-text">
-                        <h6>Soeng Souy</h6>
+                        <h6>{{Auth::user()->name}}</h6>
                         <p class="text-muted mb-0">Administrator</p>
                     </div>
-                </div> <a class="dropdown-item" href="profile.html">My Profile</a> <a class="dropdown-item" href="settings.html">Account Settings</a> 
+                </div> <a class="dropdown-item" href="{{route('profile')}}">My Profile</a> <a class="dropdown-item" href="settings.html">Account Settings</a> 
                 
                 {{-- <a class="dropdown-item" href="login.html">Logout</a>  --}}
 
